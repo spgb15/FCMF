1eead531a8ba3f90b297387fa62c300281f3bc3a
Sergio Paul Guerra Boccanedes <guerrapaul22@gmail.com>
2023-07-23 14:48:52 -0500

    Creacion de la conexion con la bd
    Se realiza la clase para la creacion de la base de datos.

---
+++ CapaDatos/CD_Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class CD_connection
+    {
+        static string server = "LAPTOP-JSLER5RB"; //Nombre del servidor de la base de datos
+
+        //String para hacer la conexion con la BD
+        private SqlConnection conexion = new SqlConnection("Server= " + server + "; Database = DB_ReinaFacultad; Integrated Security=true");
+
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+            return conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+            return conexion;
+        }
+    }
+}
